fe43df836d8da306062e20d9279bebd3ed116ff8
Skyler Riggle <skyler32702@gmail.com>
2022-10-10 15:12:08 -0500

    Stop Tracking .env

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* TEMPLATE ROUTE */
 router.get('/', function(req, res, next) {
   res.status(200).send({Hello: "World"})
 });
