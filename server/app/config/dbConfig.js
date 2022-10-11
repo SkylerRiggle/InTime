229789c8f55bbd7108191b6e1b8fcf92cba5997c
Skyler Riggle <skyler32702@gmail.com>
2022-10-11 16:09:47 -0500

    Default Password

--- server/app/config/dbConfig.js
+++ server/app/config/dbConfig.js
@@ -10,7 +10,7 @@ dbConfig = {};
 
 dbConfig.name = process.env.DB ? process.env.DB : "intime";
 dbConfig.user = process.env.USER ? process.env.USER : "postgres";
-dbConfig.pass = process.env.PASSWORD;
+dbConfig.pass = process.env.PASSWORD ? process.env.PASSWORD : "";
 dbConfig.host = process.env.HOST ? process.env.HOST : "localhost";
 dbConfig.dialect = process.env.DIALECT ? process.env.DIALECT : "postgres";
 dbConfig.operatorAliases = false;
