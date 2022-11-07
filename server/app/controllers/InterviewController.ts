074afb2077db78934c23cd43c3cd3b3c284b7956
Skyler Riggle <skyler32702@gmail.com>
2022-11-07 13:10:07 -0600

    Changed Typescript to JS

--- server/app/controllers/InterviewController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-const db = require('../config/db');
-const Interview = db.Interview;
-
-exports.get = async (req: Request, res: Response) => { }
-exports.getAll = async (req: Request, res: Response) => { }
-exports.create = async (req: Request, res: Response) => { }
-exports.edit = async (req: Request, res: Response) => { }
-exports.delete = async (req: Request, res: Response) => { }
---
+++ server/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+const db = require('../config/db');
+const User = db.User;
+
+/** Get a specific user from the database */
+exports.get = async (req, res) => { 
+    const id = req.params.id;
+}
+
+/** Get all users from the database */
+exports.getAll = async (req, res) => { }
+
+/** Handle the login sequence for a user */
+exports.login = async (req, res) => { }
+
+/** Create a new user */
+exports.create = async (req, res) => { }
+
+/** Edit an existing user */
+exports.edit = async (req, res) => { }
+
+/** Delete a user from the database */
+exports.delete = async (req, res) => { }
