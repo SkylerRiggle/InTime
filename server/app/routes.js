3fae9b494d5cf434e1af03850ebb33c97eef8a55
Skyler Riggle <skyler32702@gmail.com>
2022-11-07 12:29:28 -0600

    User Route Definitions

--- server/app/routes.js
+++ server/app/routes.js
@@ -22,8 +22,8 @@ router.get('/api/users/:id', async (req, res) => await UserController.get(req, r
 router.get('/api/users', async (req, res) => await UserController.getAll(req, res));
 router.post('/api/users/login', async (req, res) => await UserController.login(req, res));
 router.post('/api/users/create', async (req, res) => await UserController.create(req, res));
-router.post('/api/users/edit', async (req, res) => await UserController.edit(req, res));
-router.post('/api/users/delete', async (req, res) => await UserController.delete(req, res));
+router.put('/api/users/edit', async (req, res) => await UserController.edit(req, res));
+router.delete('/api/users/delete', async (req, res) => await UserController.delete(req, res));
 
 /**
  * Company routes.
@@ -32,8 +32,8 @@ router.post('/api/users/delete', async (req, res) => await UserController.delete
 router.get('/api/companies/:id', async (req, res) => await CompanyController.get(req, res));
 router.get('/api/companies', async (req, res) => await CompanyController.getAll(req, res));
 router.post('/api/companies/create', async (req, res) => await CompanyController.create(req, res));
-router.post('/api/companies/edit', async (req, res) => await CompanyController.edit(req, res));
-router.post('/api/companies/delete', async (req, res) => await CompanyController.delete(req, res));
+router.put('/api/companies/edit', async (req, res) => await CompanyController.edit(req, res));
+router.delete('/api/companies/delete', async (req, res) => await CompanyController.delete(req, res));
  
 /**
  * Interview routes.
@@ -42,8 +42,8 @@ router.post('/api/companies/delete', async (req, res) => await CompanyController
 router.get('/api/interviews/:id', async (req, res) => await InterviewController.get(req, res));
 router.get('/api/interviews', async (req, res) => await InterviewController.getAll(req, res));
 router.post('/api/interviews/create', async (req, res) => await InterviewController.create(req, res));
-router.post('/api/interviews/edit', async (req, res) => await InterviewController.edit(req, res));
-router.post('/api/interviews/delete', async (req, res) => await InterviewController.delete(req, res));
+router.put('/api/interviews/edit', async (req, res) => await InterviewController.edit(req, res));
+router.delete('/api/interviews/delete', async (req, res) => await InterviewController.delete(req, res));
  
 /**
  * Data point routes.
@@ -52,8 +52,8 @@ router.post('/api/interviews/delete', async (req, res) => await InterviewControl
 router.get('/api/datapoints/:id', async (req, res) => await DataPointController.get(req, res));
 router.get('/api/datapoints', async (req, res) => await DataPointController.getAll(req, res));
 router.post('/api/datapoints/create', async (req, res) => await DataPointController.create(req, res));
-router.post('/api/datapoints/edit', async (req, res) => await DataPointController.edit(req, res));
-router.post('/api/datapoints/delete', async (req, res) => await DataPointController.delete(req, res));
+router.put('/api/datapoints/edit', async (req, res) => await DataPointController.edit(req, res));
+router.delete('/api/datapoints/delete', async (req, res) => await DataPointController.delete(req, res));
  
 /**
  * Export application routes.
