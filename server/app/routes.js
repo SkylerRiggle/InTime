9e04b6a098fd933dc2962bd88f3ae632ba3cd26b
Skyler Riggle <skyler32702@gmail.com>
2022-11-07 06:44:18 -0600

    Minor Route Formatting

--- server/app/routes.js
+++ server/app/routes.js
@@ -29,30 +29,30 @@ router.post('/api/users/delete', async (req, res) => await UserController.delete
  * Company routes.
  */
 
- router.get('/api/companies/:id', async (req, res) => await CompanyController.get(req, res));
- router.get('/api/companies', async (req, res) => await CompanyController.getAll(req, res));
- router.post('/api/companies/create', async (req, res) => await CompanyController.create(req, res));
- router.post('/api/companies/edit', async (req, res) => await CompanyController.edit(req, res));
+router.get('/api/companies/:id', async (req, res) => await CompanyController.get(req, res));
+router.get('/api/companies', async (req, res) => await CompanyController.getAll(req, res));
+router.post('/api/companies/create', async (req, res) => await CompanyController.create(req, res));
+router.post('/api/companies/edit', async (req, res) => await CompanyController.edit(req, res));
 router.post('/api/companies/delete', async (req, res) => await CompanyController.delete(req, res));
  
 /**
  * Interview routes.
  */
 
- router.get('/api/interviews/:id', async (req, res) => await InterviewController.get(req, res));
- router.get('/api/interviews', async (req, res) => await InterviewController.getAll(req, res));
- router.post('/api/interviews/create', async (req, res) => await InterviewController.create(req, res));
- router.post('/api/interviews/edit', async (req, res) => await InterviewController.edit(req, res));
+router.get('/api/interviews/:id', async (req, res) => await InterviewController.get(req, res));
+router.get('/api/interviews', async (req, res) => await InterviewController.getAll(req, res));
+router.post('/api/interviews/create', async (req, res) => await InterviewController.create(req, res));
+router.post('/api/interviews/edit', async (req, res) => await InterviewController.edit(req, res));
 router.post('/api/interviews/delete', async (req, res) => await InterviewController.delete(req, res));
  
 /**
  * Data point routes.
  */
 
- router.get('/api/datapoints/:id', async (req, res) => await DataPointController.get(req, res));
- router.get('/api/datapoints', async (req, res) => await DataPointController.getAll(req, res));
- router.post('/api/datapoints/create', async (req, res) => await DataPointController.create(req, res));
- router.post('/api/datapoints/edit', async (req, res) => await DataPointController.edit(req, res));
+router.get('/api/datapoints/:id', async (req, res) => await DataPointController.get(req, res));
+router.get('/api/datapoints', async (req, res) => await DataPointController.getAll(req, res));
+router.post('/api/datapoints/create', async (req, res) => await DataPointController.create(req, res));
+router.post('/api/datapoints/edit', async (req, res) => await DataPointController.edit(req, res));
 router.post('/api/datapoints/delete', async (req, res) => await DataPointController.delete(req, res));
  
 /**
--- server/app/routes/user.js
+++
@@ -1,6 +0,0 @@
-//routes for CRUD ops on users & user info
-
-const express = require("express");
-const router = express.Router();
-
-module.exports = router;
