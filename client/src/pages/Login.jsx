c97a38265238d6b26d7f441c537d1d41fdfc9025
Skyler Riggle <skyler32702@gmail.com>
2022-11-07 14:50:32 -0600

    Moved Login Page to /Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,11 +10,32 @@
 const Login = () => {
   return (
     <>
-      <div class="container-fluid">
-        <h1>My First Bootstrap Page</h1>
-        <p>This is some text.</p>
+      <div
+        className="d-flex flex-column border border-success rounded container p-3 ps-5 pe-5"
+        style={{ maxWidth: '500px' }}>
+        <h5 class="pb-1 display-4">
+          InTime User Login
+        </h5>
+
+        <hr/>
+
+        <h5>Enter Account Information:</h5>
+
+        <label>Username/Email</label>
+        <input type="text" className="form-control"/>
+        <br />
+        
+        <label>Password</label>
+        <input type="password" className="form-control" />
+        <br/>
+
+        <button className="btn btn-primary">Log In</button>
+        <a href="/" className="text-muted">Forgot Password?</a>
+        <br />
+        
+        <small className="fs-6">Don't have an account?</small>
+        <button className="btn btn-outline-danger">Create Account</button>
       </div>
-      Login Page
     </>
   );
 };
