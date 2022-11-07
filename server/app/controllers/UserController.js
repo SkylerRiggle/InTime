3a9d33dcadeb130bc8fa8e2474398941014bf611
Skyler Riggle <skyler32702@gmail.com>
2022-11-07 14:18:07 -0600

    All basic User routes

--- server/app/controllers/UserController.js
+++ server/app/controllers/UserController.js
@@ -1,30 +1,43 @@
 const db = require('../config/db');
 const User = db.User;
 
-/** Get a specific user from the database */
+/** 
+ * Get a specific user from the database 
+ */
 exports.get = async (req, res) => { 
-    console.log("I MADE IT HERE")
+    // Get the route id parameter
     const id = req.params.id;
+
+    // Gather all users with the matching id (should be unique)
     const user = await User.findAll({
         where: {
             id: id
         }
     });
 
+    // User not found case
     if (!user) return res.status(404).send("User not found");
+
+    // Return the user
     return res.status(200).send(user);
 }
 
-/** Get all users from the database */
+/** 
+ * Get all users from the database 
+ */
 exports.getAll = async (req, res) => {
     const users = await User.findAll();
     return res.status(200).send(users);
 }
 
-/** Handle the login sequence for a user */
+/** 
+ * Handle the login sequence for a user 
+ */
 exports.login = async (req, res) => { }
 
-/** Create a new user */
+/** 
+ * Create a new user 
+ */
 exports.create = async (req, res) => {
     const newUser = await User.create({
         username: req.body.username,
@@ -35,10 +48,15 @@ exports.create = async (req, res) => {
     return res.status(200).send(newUser);
 }
 
-/** Edit an existing user */
+/** 
+ * Edit an existing user 
+ */
 exports.edit = async (req, res) => {
+    // Get the route id parameter
     const id = req.params.id;
+    
     try {
+        // Try to edit the user
         const user = await User.update(req.body, {
             where: {
                 id: id
@@ -46,14 +64,20 @@ exports.edit = async (req, res) => {
         });
         return res.status(200).send("User updated");
     } catch (error) {
+        // Error case
         return res.status(500).send(`Error updating user: ${error}`);
     }
 }
 
-/** Delete a user from the database */
+/** 
+ * Delete a user from the database 
+ */
 exports.delete = async (req, res) => {
+    // Get the route id parameter
     const id = req.params.id;
+
     try {
+        // Try to delete the user
         await User.destroy({
             where: {
                 id: id
@@ -61,6 +85,7 @@ exports.delete = async (req, res) => {
         });
         return res.status(200).send("User deleted");
     } catch (error) {
+        // Error case
         return res.status(500).send(`Error deleting user: ${error}`);
     }
 }
