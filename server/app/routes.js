9cff76e3c7e11c0b4e0cc5ba5c4d9f4d354793fa
CarbonellKatie <72585228+CarbonellKatie@users.noreply.github.com>
2022-11-07 20:39:03 -0600

    setup controller & individual routers

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,62 +1,21 @@
-/**
- * Module dependencies.
- */
+var express = require("express");
+var router = express.Router();
 
-const express = require('express');
-const router = express.Router();
+var loginRouter = require("./routes/login.js");
+var userRouter = require("./routes/user.js");
+var interviewRouter = require("./routes/interview.js");
+var companyRouter = require("./routes/company.js");
 
-/**
- * Controller imports.
- */
+router.use("/login", loginRouter);
+router.use("/user", userRouter);
+router.use("/interview", interviewRouter);
+router.use("/company", companyRouter);
 
-const UserController = require('./controllers/UserController');
-const CompanyController = require('./controllers/CompanyController');
-const InterviewController = require('./controllers/InterviewController');
-const DataPointController = require('./controllers/DataPointController');
+/* TEMPLATE ROUTE */
+router.get("/", async function (req, res, next) {
+  res.status(200).send({ Example: "data" });
+});
 
-/**
- * User routes.
- */
-
-router.get('/api/users/:id', async (req, res) => await UserController.get(req, res));
-router.get('/api/users', async (req, res) => await UserController.getAll(req, res));
-router.post('/api/users/login', async (req, res) => await UserController.login(req, res));
-router.post('/api/users/create', async (req, res) => await UserController.create(req, res));
-router.put('/api/users/:id', async (req, res) => await UserController.edit(req, res));
-router.delete('/api/users/:id', async (req, res) => await UserController.delete(req, res));
-
-/**
- * Company routes.
- */
-
-router.get('/api/companies/:id', async (req, res) => await CompanyController.get(req, res));
-router.get('/api/companies', async (req, res) => await CompanyController.getAll(req, res));
-router.post('/api/companies/create', async (req, res) => await CompanyController.create(req, res));
-router.put('/api/companies/:id', async (req, res) => await CompanyController.edit(req, res));
-router.delete('/api/companies/:id', async (req, res) => await CompanyController.delete(req, res));
- 
-/**
- * Interview routes.
- */
-
-router.get('/api/interviews/:id', async (req, res) => await InterviewController.get(req, res));
-router.get('/api/interviews', async (req, res) => await InterviewController.getAll(req, res));
-router.post('/api/interviews/create', async (req, res) => await InterviewController.create(req, res));
-router.put('/api/interviews/:id', async (req, res) => await InterviewController.edit(req, res));
-router.delete('/api/interviews/:id', async (req, res) => await InterviewController.delete(req, res));
- 
-/**
- * Data point routes.
- */
-
-router.get('/api/datapoints/:id', async (req, res) => await DataPointController.get(req, res));
-router.get('/api/datapoints', async (req, res) => await DataPointController.getAll(req, res));
-router.post('/api/datapoints/create', async (req, res) => await DataPointController.create(req, res));
-router.put('/api/datapoints/:id', async (req, res) => await DataPointController.edit(req, res));
-router.delete('/api/datapoints/:id', async (req, res) => await DataPointController.delete(req, res));
- 
-/**
- * Export application routes.
- */
+router.use();
 
 module.exports = router;
