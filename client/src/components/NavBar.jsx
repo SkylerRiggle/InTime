dbedbb8642aa7d518afe190c9c3c105ef7d8cc4d
Skyler Riggle <skyler32702@gmail.com>
2022-11-15 21:58:27 -0600

    Company Page V1

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () =>
  {
    return (
      <div
-       className='p-1 ps-4 d-flex align-items-bottom'
+       className='p-1 ps-4'
        style={{ width: '100vw', height: '48pt', boxShadow:'0 -15px 30px black' }}>
        <a href='/'><img src={Logo} alt="Logo.png" height="80pt"/></a>
     </div>
--- client/src/pages/Company.jsx
+++ client/src/pages/Company.jsx
@@ -6,14 +6,37 @@
  * as well as any other introductory data for the organization.
  */
 
+import Logo from '../images/logo.png';
+
 const Company = () => {
   return (
     <>
-      <div>
-
+      <div className='d-flex align-items-end mt-5 mb-5'>
+        <div className='d-flex align-items-center justify-content-center rounded p-1 me-3'
+          style={{ width: '100pt', height: '100pt', border:'2pt solid black' }}>
+          <img src={Logo} alt="Company-Logo" width="100%" />
+        </div>
+        <div className='w-75'>
+          <h2>COMPANY NAME</h2><hr/>
+        </div>
       </div>
 
-      <hr/>
+      <h4>Internships:</h4>
+      <div className='d-flex flex-column align-items-center w-100 border rounded'
+        style={{overflowY:'scroll', maxHeight:'50vh'}}>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+        <h1>IM AN EXAMPLE</h1>
+      </div>
     </>
   );
 };
