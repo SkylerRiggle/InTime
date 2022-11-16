331058c36444200040bf1d9353cd385040c807c7
Skyler Riggle <skyler32702@gmail.com>
2022-11-15 22:17:00 -0600

    Login Link In NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,9 +4,13 @@ const NavBar = () =>
  {
    return (
      <div
-       className='p-1 ps-4'
+       className='d-flex justify-content-between p-1 ps-4 pe-4'
        style={{ width: '100%', height: '48pt', boxShadow:'0 -15px 30px black' }}>
-       <a href='/'><img src={Logo} alt="Logo.png" height="80pt"/></a>
+       <a href='/'><img src={Logo} alt="Logo.png" height="80pt" /></a>
+       
+       <div className='d-flex h-100 align-items-center'>
+         <a href='/login' className='btn btn-success m-2'>Login</a>
+       </div>
     </div>
    );
  };
