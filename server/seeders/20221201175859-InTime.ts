6fa4c97dcbb84a98788bfebe540140ce3f49ba1b
Skyler Riggle <skyler32702@gmail.com>
2022-12-01 12:05:18 -0600

    Committed to Seeder Design

---
+++ server/seeders/20221201175859-InTime.ts
@@ -0,0 +1,11 @@
+import { QueryInterface } from 'sequelize';
+
+/** Upstream Seed */
+export const up = async (queryInterface: QueryInterface) => {
+
+}
+
+/** Downstream Seed */
+export const down = async (queryInterface: QueryInterface) => {
+
+}
--- server/src/seeders/testSeeder.ts
+++
@@ -1,5 +0,0 @@
-/** Model Imports */
-import User from "../models/User";
-import Company from "../models/Company";
-import Interview from "../models/Interview";
-import Data from "../models/Data";
