6db2dafd8b5d8be72cf97d8a771b7a14779ee157
Skyler Riggle <skyler32702@gmail.com>
2022-12-01 13:59:01 -0600

    Finalized Seeder

--- server/bin/seed.ts
+++ server/bin/seed.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import db from '../src/config/db';
-import { up } from '../seeders/Test-InTime';
+import { up, down } from '../seeders/Test-InTime';
 
 /**
  * Configure environment constiables
@@ -13,15 +13,20 @@ dotenv.config();
 db.sync().then(() => {
     console.log("\u001B[32m" + "Database Successfully Synced.");
     console.log("\u001B[0m" + "----------------------------------");
-    up().then(() => {
-        console.log("\u001B[32m" + "Database Successfully Seeded.");
-        console.log("\u001B[0m" + "----------------------------------");
+    down().then(() => { // Clear previous entries to avoid issues
 
-        process.kill(process.pid);
+        up().then(() => { // Handle Seeding
+            console.log("\u001B[32m" + "Database Successfully Seeded.");
+            console.log("\u001B[0m" + "----------------------------------");
+
+            process.kill(process.pid);
+        }).catch((error) => {
+            console.error("Issue while seeding database: ", error);
+            process.kill(process.pid);
+        })
     }).catch((error) => {
         console.error("Issue while seeding database: ", error);
-        process.kill(process.pid);
-    })
+    });
 }).catch((error) => {
     console.error("Unable to sync with database: ", error);
     process.kill(process.pid);
--- server/seeders/Test-InTime.ts
+++ server/seeders/Test-InTime.ts
@@ -42,102 +42,105 @@ export const up = async () => {
 
   await Data.bulkCreate([{
     date: new Date(2010, 5, 12),
-    daysSinceApplication: 10,
+    daysSinceApplication: 5,
     responseData: {},
-    eventId: interviews[0],
-    userId: users[1]
+    eventId: interviews[0].id,
+    userId: users[1].id
   }, {
     date: new Date(2010, 6, 5),
-    daysSinceApplication: 10,
+    daysSinceApplication: 8,
     responseData: {},
-    eventId: interviews[0],
-    userId: users[0]
+    eventId: interviews[0].id,
+    userId: users[0].id
   }, {
     date: new Date(2010, 6, 10),
-    daysSinceApplication: 10,
+    daysSinceApplication: 11,
     responseData: {},
-    eventId: interviews[0],
-    userId: users[1]
+    eventId: interviews[0].id,
+    userId: users[1].id
   }, {
     date: new Date(2010, 8, 3),
-    daysSinceApplication: 10,
+    daysSinceApplication: 13,
     responseData: {},
-    eventId: interviews[0],
-    userId: users[0]
+    eventId: interviews[0].id,
+    userId: users[0].id
   }, {
     date: new Date(2010, 8, 16),
     daysSinceApplication: 10,
     responseData: {},
-    eventId: interviews[0],
-    userId: users[0]
+    eventId: interviews[0].id,
+    userId: users[0].id
+  },
+    
+  {
+    date: new Date(2010, 5, 12),
+    daysSinceApplication: 24,
+    responseData: {},
+    eventId: interviews[1].id,
+    userId: users[1].id
+  }, {
+    date: new Date(2010, 6, 5),
+    daysSinceApplication: 9,
+    responseData: {},
+    eventId: interviews[1].id,
+    userId: users[0].id
+  }, {
+    date: new Date(2010, 6, 10),
+    daysSinceApplication: 12,
+    responseData: {},
+    eventId: interviews[1].id,
+    userId: users[1].id
+  }, {
+    date: new Date(2010, 8, 3),
+    daysSinceApplication: 19,
+    responseData: {},
+    eventId: interviews[1].id,
+    userId: users[1].id
+  }, {
+    date: new Date(2010, 8, 16),
+    daysSinceApplication: 7,
+    responseData: {},
+    eventId: interviews[1].id,
+    userId: users[0].id
   },
     
   {
     date: new Date(2010, 5, 12),
     daysSinceApplication: 10,
     responseData: {},
-    eventId: interviews[1],
-    userId: users[1]
+    eventId: interviews[2].id,
+    userId: users[0].id
   }, {
     date: new Date(2010, 6, 5),
     daysSinceApplication: 10,
     responseData: {},
-    eventId: interviews[1],
-    userId: users[0]
+    eventId: interviews[2].id,
+    userId: users[0].id
   }, {
     date: new Date(2010, 6, 10),
-    daysSinceApplication: 10,
+    daysSinceApplication: 4,
     responseData: {},
-    eventId: interviews[1],
-    userId: users[1]
+    eventId: interviews[2].id,
+    userId: users[1].id
   }, {
     date: new Date(2010, 8, 3),
-    daysSinceApplication: 10,
+    daysSinceApplication: 12,
     responseData: {},
-    eventId: interviews[1],
-    userId: users[1]
+    eventId: interviews[2].id,
+    userId: users[1].id
   }, {
     date: new Date(2010, 8, 16),
-    daysSinceApplication: 10,
+    daysSinceApplication: 14,
     responseData: {},
-    eventId: interviews[1],
-    userId: users[0]
-  },
-    
-  {
-    date: new Date(2010, 5, 12),
-    daysSinceApplication: 10,
-    responseData: {},
-    eventId: interviews[2],
-    userId: users[0]
-  }, {
-    date: new Date(2010, 6, 5),
-    daysSinceApplication: 10,
-    responseData: {},
-    eventId: interviews[2],
-    userId: users[0]
-  }, {
-    date: new Date(2010, 6, 10),
-    daysSinceApplication: 10,
-    responseData: {},
-    eventId: interviews[2],
-    userId: users[1]
-  }, {
-    date: new Date(2010, 8, 3),
-    daysSinceApplication: 10,
-    responseData: {},
-    eventId: interviews[2],
-    userId: users[1]
-  }, {
-    date: new Date(2010, 8, 16),
-    daysSinceApplication: 10,
-    responseData: {},
-    eventId: interviews[2],
-    userId: users[1]
+    eventId: interviews[2].id,
+    userId: users[1].id
   }]);
 }
 
 /** Downstream Seed */
 export const down = async () => {
-  console.log("UNSEEDING")
+  await Data.destroy({ where: {}, cascade: true, truncate: true });
+  await Company.destroy({ where: {}, cascade: true, truncate: true });
+  await Interview.destroy({ where: {}, cascade: true, truncate: true });
+  await User.destroy({ where: {}, cascade: true, truncate: true });
 }
