4b1e0f3cd526fe7dddbd10bbce97310a6578737b
Skyler Riggle <skyler32702@gmail.com>
2022-12-01 12:25:58 -0600

    Model Import

--- server/seeders/Test-InTime.ts
+++ server/seeders/Test-InTime.ts
@@ -1,3 +1,9 @@
+/** Model Imports */
+import User from "../src/models/User";
+import Company from "../src/models/Company";
+import Interview from "../src/models/Interview";
+import Data from "../src/models/Data";
+
 /** Upstream Seed */
 export const up = async () => {
   console.log("SEEDING")
