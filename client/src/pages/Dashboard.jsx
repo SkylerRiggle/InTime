f7b42d115b1ae962216b8717b27e76b0f9dcbc76
CarbonellKatie <72585228+CarbonellKatie@users.noreply.github.com>
2022-12-02 15:32:21 -0600

    finalize db

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,18 +1,21 @@
-/** 
+/**
  * Route: /:id
- * 
- * The dashboard page will allow users to search 
+ *
+ * The dashboard page will allow users to search
  * for reviews and companys, as well as manage their accounts.
  */
 
- const Dashboard = () => 
- {
-   return (
-     <>
-        Dashboard Page
-     </>
-   );
- };
- 
- export default Dashboard;
- 
+const Dashboard = () => {
+  return (
+    <>
+      Dashboard Page
+      <button id="view-companies">View Companies</button>
+      <table>
+        <td>Amazon</td>
+        <td>Apple</td>
+      </table>
+    </>
+  );
+};
+
+export default Dashboard;
